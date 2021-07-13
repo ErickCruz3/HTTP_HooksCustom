977df7fb5ac91a0edd544b5e951754dde1b0ad31
ErickCruz3 <30erickC@gmail.com>
2021-07-13 11:07:08 -0500

    hook http

--- src/components/NewTask/NewTask.js
+++ src/components/NewTask/NewTask.js
@@ -12,7 +12,7 @@ const NewTask = (props) => {
     setError(null);
     try {
       const response = await fetch(
-        'https://react-http-6b4a6.firebaseio.com/tasks.json',
+        'https://http-react-93723-default-rtdb.firebaseio.com//tasks.json',
         {
           method: 'POST',
           body: JSON.stringify({ text: taskText }),
